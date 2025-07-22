691c29bf059b1b621757895d1ff8735e05352fd3
sungjiin1064 <psj10645001@gmail.com>
2025-07-22 15:17:48 +0900

    (07-21)유니티 충돌 스크럽팅

--- unity_basic2D/Assets/TriggerCheck.cs
+++ unity_basic2D/Assets/TriggerCheck.cs
@@ -13,11 +13,11 @@ public class TriggerCheck : MonoBehaviour
             Destroy(collision.gameObject); // 충돌한 대상을 없애라
             //Destroy(gameObject); //부딛힌 자신을 없애라(isTrigger체크) 
         }
-        if(collision.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
             Debug.Log("충돌한 오브젝트의 이름 : " + collision.gameObject.name);
         }
-        
+
     }
 
 }
